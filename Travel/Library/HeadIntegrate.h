3a2eea854b3a3ba84c44f0c520c75b75dfa6cac1
samuel <fm9917@gmail.com>
2013-01-16 22:17:02 +0800

    测试地址URL
    测试地址URL

--- Travel/Library/HeadIntegrate.h
+++ Travel/Library/HeadIntegrate.h
@@ -10,7 +10,8 @@
 #define RGBCOLOR(r,g,b) [UIColor colorWithRed:(r)/255.0 green:(g)/255.0 blue:(b)/255.0 alpha:1]
 //系统默认的导航按钮
 #define BARBUTTON(TITLE, SELECTOR)[[[UIBarButtonItem alloc] initWithTitle:TITLE style:UIBarButtonItemStylePlain target:self action:SELECTOR] autorelease]
-#define API_SEAECHSERVER_ADR (@"http://localhost:8080/yun/")
+#define API_SEAECHSERVER_ADR (@"http://yun.youcn.com.cn/")          //线上地址
+//#define API_SEAECHSERVER_ADR (@"http://localhost:8080/yun/")      //本地测试地址
 #define NETWORK_BAD (@"您的网络好像有点问题，请重试")
 
 @interface HeadIntegrate : NSObject
